d0f235cc0165f4ebfbaf1739b54b310a6eeed7b8
Vishay142 <47138413+Vishay142@users.noreply.github.com>
2019-10-14 12:34:01 +0200

    opdracht 2

--- opdracht2.php
+++ opdracht2.php
@@ -1,1 +1,53 @@
 <?php
+
+?>
+
+<html>
+<head>
+    <style>
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 20%;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .oranje {
+            text-align: center;
+            color: #ff8400;
+
+        }
+    </style>
+
+<img src="img/monkey-business.jpg">
+<h1>Select your monkey!</h1>
+<br><br>
+<img src="img/monkey_swings.png">
+</head>
+<br>
+
+<body>
+<div class="oranje">
+    <a href="https://www.google.nl/search?q=Baviaan&tbm=isch">Baviaan</a> <br>
+    <a href="https://www.google.nl/search?q=Guereza&tbm=isch">Guereza</a> <br>
+    <a href="https://www.google.nl/search?q=Langoer&tbm=isch">Langoer</a> <br>
+    <a href="https://www.google.nl/search?q=Neusaap&tbm=isch">Neusaap</a> <br>
+    <a href="https://www.google.nl/search?q=Tamarin&tbm=isch">Tamarin</a> <br>
+    <a href="https://www.google.nl/search?q=Brulaap&tbm=isch">Brulaap</a> <br>
+    <a href="https://www.google.nl/search?q=Halfaap&tbm=isch">Halfaap</a> <br>
+    <a href="https://www.google.nl/search?q=Madril&tbm=isch">Mandril</a> <br>
+</div>
+</body>
+
+
+
+
+
+
+
+</html>
+
